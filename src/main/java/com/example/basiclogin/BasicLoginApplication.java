ff8598c19da1d90fa641a34c06f04a10910985af
Kwon042 <miri9612@gmail.com>
2025-06-16 19:59:56 -0700

    basiclogin 기초설계 - BasicLogin 대문자

--- src/main/java/com/example/basiclogin/BasicLoginApplication.java
+++ src/main/java/com/example/basiclogin/BasicLoginApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicloginApplication {
+public class BasicLoginApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BasicloginApplication.class, args);
+		SpringApplication.run(BasicLoginApplication.class, args);
 	}
 
 }
